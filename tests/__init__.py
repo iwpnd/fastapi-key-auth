466b9632618542245120f8bb4ad1669fb2b2674a
Benjamin Ramser <legionaerr@googlemail.com>
2024-01-09 15:41:13 +0100

    feat: export helper utils
    and trigger python-semantic-release to pypi O_O

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,5 @@
 from fastapi_key_auth.dependency import AuthorizerDependency
 from fastapi_key_auth.middleware import AuthorizerMiddleware
+from fastapi_key_auth.utils import get_api_keys_in_env, get_default_api_key_pattern
 
 __all__ = ["AuthorizerMiddleware", "AuthorizerDependency"]
